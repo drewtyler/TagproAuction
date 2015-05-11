fe2ac9d2434104739b68a36971efba42ab16656e
drewtyler <drew@drewtyler.com>
2015-05-10 17:53:28 -0700

    changed team names

--- client/views/links/links.js
+++ client/views/links/links.js
@@ -18,7 +18,7 @@ Template.links.helpers({
         return true;
     if(Administrators.find({"username":username}).count()>0)
         return true;
-    if(Meteor.user().username == "BBQChicken")
+    if(Meteor.user().username == "Spiller" || Meteor.user().username == "dodsfall" || Meteor.user().username == "Nawse" || Meteor.user().username == "Xile")
         return true;
     return false;
   }
