a2815cea25cc512aa38c22824af7c0ef78b08204
drewtyler <drew@drewtyler.com>
2015-05-17 16:58:18 -0700

    fixing draft bug

--- client/views/trade/trade.js
+++ client/views/trade/trade.js
@@ -27,5 +27,47 @@ Template.trade.helpers({
 Template.trade.events({
     'change #proposedCaptain' : function() {
         Session.set("tradeCaptainToView", $("#proposedCaptain").val());
+    },
+    'submit':function(event) {
+        tgt = event.tgt;
+        tradeProposal = {};
+        myName = Meteor.user().username;
+        theirName = $("#proposedCaptain").val();
+
+        tradeProposal.proposingCaptain = myName;
+        tradeProposal.proposedCaptain = theirName;
+
+        if(theirName == "Select" || theirName == "Select Captain") {
+            // TODO: "You can't do that";
+            return false;
+        }
+        myTeamName = TeamNames.findOne({"captain":myName}).teamname;
+        theirTeamName = TeamNames.findOne({"captain":theirName}).teamname;
+        myPlayerCount = TeamData.find({"teamname":myTeamName,"name":{$ne:""}}).count();
+        theirPlayerCount = TeamData.find({"teamname":theirTeamName,"name":{$ne:""}}).count();
+
+        tradeProposal.proposingPlayers = [];
+        tradeProposal.proposedPlayers = [];
+        for(curplayer = 2; curplayer <= myPlayerCount; curplayer++) {
+            if($(tgt).find('[name=myplayer' + curplayer + ']').is(':checked')) {
+                tradeProposal.proposingPlayers.push(curplayer);
+            }
+        }
+        for(curplayer = 2; curplayer <= myPlayerCount; curplayer++) {
+            if($(tgt).find('[name=theirplayer' + curplayer + ']').is(':checked')) {
+                tradeProposal.proposedPlayers.push(curplayer);
+            }
+        }
+
+        tradeProposal.proposingAmount = $(tgt).tagCoinsToGive.value;
+        tradeProposal.proposedAmount = $(tgt).tagCoinsToReceive.value;
+        tradeProposal.reasoning = $(tgt).reasoning.value;
+        tradeProposal.message = $(tgt).message.value;
+
+        console.log(tradeProposal);
+        return false;
+        // know opposing captain and can figure out opposing team name/my team name
+        // then gotta loop through each player on each team and see if that player is selected
+        // then add in messages and put it in the db
     }
 });
